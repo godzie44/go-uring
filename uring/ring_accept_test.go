a9cd5f732c9c2c9bf94b777a21ee1fd0c602fb57
godzie44 <godzie@yandex.ru>
2021-09-28 12:47:17 +0300

    add TestAcceptMany in ring_accept_test.go

--- uring/ring_accept_test.go
+++ uring/ring_accept_test.go
@@ -30,6 +30,7 @@ func getBlockingFd(t *testing.T, conn syscall.Conn) (fdescr uintptr) {
 
 const sendData = "hello world"
 
+// Test that IORING_OP_ACCEPT works.
 func TestAccept(t *testing.T) {
 	ring, err := NewRing(64)
 	require.NoError(t, err)
@@ -111,6 +112,7 @@ func queueRecv(ring *URing, fd uintptr, buff []byte) error {
 	return ring.FillNextSQE(cmd.fillSQE)
 }
 
+// Test we can cancel IORING_OP_ACCEPT.
 func TestAcceptCancel(t *testing.T) {
 	type testCase struct {
 		cancelDelay time.Duration
@@ -158,5 +160,51 @@ func TestAcceptCancel(t *testing.T) {
 
 		require.NoError(t, ring.Close())
 		require.NoError(t, tcpListener.Close())
+	}
+}
+
+//Test issue many accepts and see if we handle cancellation on exit
+func TestAcceptMany(t *testing.T) {
+	type testCase struct {
+		count int
+		delay time.Duration
+	}
+	testCases := []testCase{
+		{1, 0},
+		{1, time.Microsecond * 10000},
+	}
+
+	for n, tc := range testCases {
+		ring, err := NewRing(uint32(2 * tc.count))
+		require.NoError(t, err)
+
+		listeners := make([]*net.TCPListener, 0, tc.count)
+		for i := 0; i < tc.count; i++ {
+			tcpListener, err := net.ListenTCP("tcp", &net.TCPAddr{Port: 8080 + (n * tc.count) + i})
+			require.NoError(t, err)
+			listeners = append(listeners, tcpListener)
+
+			listenerFd := getBlockingFd(t, tcpListener)
+			cmd := Accept(listenerFd, 0)
+			err = ring.FillNextSQE(cmd.fillSQE)
+			require.NoError(t, err)
+		}
+
+		submitted, err := ring.Submit()
+		require.NoError(t, err)
+		assert.Equal(t, uint(tc.count), submitted)
+
+		if tc.delay != 0 {
+			time.Sleep(tc.delay)
+		}
+
+		_, err = ring.PeekCQE()
+		assert.True(t, err == syscall.EAGAIN)
+
+		for _, l := range listeners {
+			assert.NoError(t, l.Close())
+		}
+
+		require.NoError(t, ring.Close())
 	}
 }
