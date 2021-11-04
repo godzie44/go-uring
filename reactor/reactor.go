70060a2dbc3ce916dc701006357dc7aa238ec62e
godzie44 <godzie@yandex.ru>
2021-11-04 12:17:03 +0300

    reactor: add ExecuteAndWaitWithDeadline method

--- reactor/reactor.go
+++ reactor/reactor.go
@@ -2,9 +2,12 @@ package uring
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/godzie44/go-uring/uring"
 	"log"
 	"runtime"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -22,11 +25,21 @@ func (r *Result) Operation() uring.Operation {
 	return r.op
 }
 
+type command struct {
+	op  uring.Operation
+	cqe chan uring.CQEvent
+}
+
 type Reactor struct {
 	ring *uring.URing
 
-	commands     map[uint64]uring.Operation
+	queueMy sync.Mutex
+
+	commands   map[uint64]*command
+	commandsMu sync.RWMutex
+
 	currentNonce uint64
+	nonceLock    sync.Mutex
 
 	result chan Result
 
@@ -41,12 +54,13 @@ func WithTickTimeout(duration time.Duration) ReactorOption {
 	}
 }
 
-func NewReactor(ring *uring.URing, opts ...ReactorOption) *Reactor {
+func New(ring *uring.URing, opts ...ReactorOption) *Reactor {
 	r := &Reactor{
 		ring:         ring,
 		result:       make(chan Result),
-		commands:     map[uint64]uring.Operation{},
-		tickDuration: time.Millisecond * 100,
+		commands:     map[uint64]*command{},
+		tickDuration: time.Millisecond * 1000,
+		currentNonce: 100,
 	}
 
 	for _, opt := range opts {
@@ -67,9 +81,20 @@ func (r *Reactor) Run(ctx context.Context) error {
 		}
 
 		if err == nil {
-			r.result <- Result{op: r.commands[cqe.UserData], err: cqe.Error()}
-			delete(r.commands, cqe.UserData)
+			//r.result <- Result{op: r.commands[cqe.UserData], err: cqe.Error()}
 			r.ring.SeenCQE(cqe)
+
+			r.commandsMu.Lock()
+			chn := r.commands[cqe.UserData].cqe
+
+			chn <- uring.CQEvent{
+				UserData: cqe.UserData,
+				Res:      cqe.Res,
+				Flags:    cqe.Flags,
+			}
+			delete(r.commands, cqe.UserData)
+
+			r.commandsMu.Unlock()
 		}
 
 		if err != nil && err != syscall.ETIME {
@@ -84,19 +109,105 @@ func (r *Reactor) Run(ctx context.Context) error {
 	}
 }
 
-func (r *Reactor) Execute(ops ...uring.Operation) error {
-	for _, op := range ops {
-		r.commands[r.currentNonce] = op
+func (r *Reactor) ExecuteAndWait(op uring.Operation) (uring.CQEvent, error) {
+	nonce := r.nextNonce()
 
-		if err := r.ring.QueueSQE(op, 0, r.currentNonce); err != nil {
-			return err
-		}
+	cqeChan := make(chan uring.CQEvent)
+	defer close(cqeChan)
 
-		r.currentNonce++
+	cmd := &command{
+		op:  op,
+		cqe: cqeChan,
 	}
 
-	_, err := r.ring.Submit()
+	r.commandsMu.Lock()
+	r.commands[nonce] = cmd
+	r.commandsMu.Unlock()
+
+	if err := r.queueSQE(sqeQueueRequest{op, 0, nonce}); err != nil {
+		return uring.CQEvent{}, err
+	}
+
+	if _, err := r.ring.Submit(); err != nil {
+		return uring.CQEvent{}, err
+	}
+
+	return <-cqeChan, nil
+}
+
+func (r *Reactor) ExecuteAndWaitWithDeadline(op uring.Operation, deadline time.Time) (uring.CQEvent, error) {
+	opNonce := r.nextNonce()
+	timeoutNonce := r.nextNonce()
+
+	cqeChan := make(chan uring.CQEvent, 2)
+	defer close(cqeChan)
+
+	cmd := &command{
+		op:  op,
+		cqe: cqeChan,
+	}
+	linkTimeoutCmd := &command{
+		op:  uring.LinkTimeout(deadline.Sub(time.Now())),
+		cqe: cqeChan,
+	}
+
+	r.commandsMu.Lock()
+	r.commands[opNonce] = cmd
+	r.commands[timeoutNonce] = linkTimeoutCmd
+	r.commandsMu.Unlock()
+
+	err := r.queueSQE(
+		sqeQueueRequest{op, uring.SqeIOLinkFlag, opNonce},
+		sqeQueueRequest{linkTimeoutCmd.op, 0, timeoutNonce},
+	)
+	if err != nil {
+		return uring.CQEvent{}, err
+	}
+
+	if _, err = r.ring.Submit(); err != nil {
+		return uring.CQEvent{}, err
+	}
+
+	opCqe, timeoutCqe := <-cqeChan, <-cqeChan
+	if opCqe.UserData != opNonce {
+		opCqe, timeoutCqe = timeoutCqe, opCqe
+	}
+
+	if timeoutCqe.Error() != nil && !errors.Is(timeoutCqe.Error(), syscall.ECANCELED) {
+		return opCqe, timeoutCqe.Error()
+	}
+	return opCqe, nil
+}
+
+type sqeQueueRequest struct {
+	op       uring.Operation
+	flags    uint8
+	userData uint64
+}
+
+func (r *Reactor) queueSQE(requests ...sqeQueueRequest) (err error) {
+	r.queueMy.Lock()
+	defer r.queueMy.Unlock()
+
+	for _, req := range requests {
+		if qErr := r.ring.QueueSQE(req.op, req.flags, req.userData); qErr != nil {
+			if err == nil {
+				err = qErr
+			} else {
+				err = fmt.Errorf("%w; %s", err, qErr.Error())
+			}
+		}
+	}
+
 	return err
+}
+
+func (r *Reactor) nextNonce() uint64 {
+	r.nonceLock.Lock()
+	r.currentNonce++
+	nonce := r.currentNonce
+	r.nonceLock.Unlock()
+	return nonce
 }
 
 func (r *Reactor) Result() <-chan Result {
--- reactor/reactor_test.go
+++ reactor/reactor_test.go
@@ -3,54 +3,118 @@ package uring
 import (
 	"context"
 	"github.com/godzie44/go-uring/uring"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"time"
+
+	"golang.org/x/sys/unix"
 	"io/ioutil"
+	"net"
 	"os"
 	"sync"
+	"syscall"
 	"testing"
-	"time"
 	"unsafe"
 )
 
-func TestReactorExecuteReadVCommand(t *testing.T) {
-	r, err := uring.NewRing(64)
-	require.NoError(t, err)
-	defer r.Close()
+type ReactorTestSuite struct {
+	suite.Suite
+	ring    *uring.URing
+	reactor *Reactor
 
-	f, err := os.Open("../go.mod")
-	require.NoError(t, err)
-	defer f.Close()
-	op, err := uring.ReadV(f, 16)
-	require.NoError(t, err)
+	stopReactor context.CancelFunc
+	wg          *sync.WaitGroup
+}
+
+func (ts *ReactorTestSuite) SetupTest() {
+	ring, err := uring.NewRing(64)
+	ts.Require().NoError(err)
+	ts.ring = ring
+
+	ts.reactor = New(ts.ring)
 
 	ctx, cancel := context.WithCancel(context.Background())
+	ts.stopReactor = cancel
 
-	reactor := NewReactor(r)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
+	ts.wg = &sync.WaitGroup{}
+	ts.wg.Add(1)
 	go func() {
-		defer wg.Done()
-		err := reactor.Run(ctx)
-		require.NoError(t, err)
+		defer ts.wg.Done()
+		err := ts.reactor.Run(ctx)
+		ts.Require().NoError(err)
 	}()
+}
 
-	err = reactor.Execute(op)
-	require.NoError(t, err)
+func (ts *ReactorTestSuite) TearDownTest() {
+	ts.stopReactor()
+	ts.wg.Wait()
 
-	select {
-	case res := <-reactor.Result():
-		assert.NoError(t, res.Error())
-		reads := res.Operation().(*uring.ReadVOp)
-		expected, err := ioutil.ReadFile("../go.mod")
-		assert.NoError(t, err)
+	ts.Require().NoError(ts.ring.Close())
+}
 
-		str := string(unsafe.Slice(reads.IOVecs[0].Base, reads.Size))
-		assert.Equal(t, string(expected), str)
-	case <-time.After(3 * time.Second):
-		assert.Fail(t, "no reads at 3 seconds")
+func (ts *ReactorTestSuite) TestReactorExecuteReadVCommand() {
+	f, err := os.Open("../go.mod")
+	ts.Require().NoError(err)
+	defer f.Close()
+
+	op, err := uring.ReadV(f, 16)
+	ts.Require().NoError(err)
+
+	cqe, err := ts.reactor.ExecuteAndWait(op)
+	ts.Require().NoError(err)
+
+	reads := op
+	expected, err := ioutil.ReadFile("../go.mod")
+	ts.Require().NoError(err)
+
+	str := string(unsafe.Slice(reads.IOVecs[0].Base, reads.Size))
+	ts.Require().Equal(string(expected), str)
+
+	ts.Require().Len(expected, int(cqe.Res))
+}
+
+func (ts *ReactorTestSuite) TestReactorExecuteWithDeadline() {
+	l, fd, err := makeTCPListener("0.0.0.0:8080")
+	ts.Require().NoError(err)
+	defer l.Close()
+
+	acceptTime := time.Now()
+	cqe, err := ts.reactor.ExecuteAndWaitWithDeadline(uring.Accept(fd, 0), acceptTime.Add(time.Second))
+
+	ts.Require().Error(err, syscall.ETIME)
+	ts.Require().Error(cqe.Error(), syscall.ECANCELED)
+	ts.Require().True(time.Now().Sub(acceptTime) > time.Second && time.Now().Sub(acceptTime) < time.Second+time.Millisecond*100)
+}
+
+func TestReactor(t *testing.T) {
+	suite.Run(t, new(ReactorTestSuite))
+}
+
+func makeTCPListener(addr string) (*net.TCPListener, uintptr, error) {
+	var fdescr uintptr
+
+	var listenConfig = net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			var err error
+			_ = c.Control(func(fd uintptr) {
+				if err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
+					return
+				}
+				if err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
+					return
+				}
+				if err = syscall.SetNonblock(int(fd), false); err != nil {
+					return
+				}
+				fdescr = fd
+			})
+			return err
+		},
 	}
 
-	cancel()
-	wg.Wait()
+	conn, err := listenConfig.Listen(context.Background(), "tcp", addr)
+	if err != nil {
+		return nil, fdescr, err
+	}
+
+	return conn.(*net.TCPListener), fdescr, err
 }
